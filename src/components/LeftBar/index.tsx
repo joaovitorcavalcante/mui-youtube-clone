9386595aa1745314719baf84c64fea49194d3f5b
JOAO VITOR CAVALCANTE BARBOSA <117310@pmenos.com.br>
2022-03-28 12:46:08 -0300

    feat: Hide LeftBar when zooming out

--- src/components/LeftBar/index.tsx
+++ src/components/LeftBar/index.tsx
@@ -3,6 +3,7 @@ import {
   Button,
   Divider,
   Drawer,
+  Hidden,
   List,
   ListItem,
   ListItemIcon,
@@ -24,114 +25,116 @@ export function LeftBar() {
   const classes = useStyles();
 
   return (
-    <Drawer
-      variant="permanent"
-      className={classes.Drawer}
-      classes={{ paper: classes.DrawerPaper }}
-    >
-      <Toolbar />
-      <Box className={classes.DrawerContainer}>
-        <List>
-          <ListItem
-            button
-            classes={{
-              root: classes.DrawerListItem,
-            }}
-          >
-            <ListItemIcon>
-              <HomeIcon />
-            </ListItemIcon>
-            <ListItemText
+    <Hidden mdDown>
+      <Drawer
+        variant="permanent"
+        className={classes.Drawer}
+        classes={{ paper: classes.DrawerPaper }}
+      >
+        <Toolbar />
+        <Box className={classes.DrawerContainer}>
+          <List>
+            <ListItem
+              button
               classes={{
-                primary: classes.DrawerListItemText,
+                root: classes.DrawerListItem,
               }}
-              primary="Início"
-            />
-          </ListItem>
-          <ListItem
-            button
-            classes={{
-              root: classes.DrawerListItem,
-            }}
-          >
-            <ListItemIcon>
-              <SubscriptionsIcon />
-            </ListItemIcon>
-            <ListItemText
-              classes={{
-                primary: classes.DrawerListItemText,
-              }}
-              primary="Em alta"
-            />
-          </ListItem>
-          <ListItem
-            button
-            classes={{
-              root: classes.DrawerListItem,
-            }}
-          >
-            <ListItemIcon>
-              <WhatshotIcon />
-            </ListItemIcon>
-            <ListItemText
-              classes={{
-                primary: classes.DrawerListItemText,
-              }}
-              primary="Inscrições"
-            />
-          </ListItem>
-        </List>
-        <Divider />
-        <List>
-          <ListItem
-            button
-            classes={{
-              root: classes.DrawerListItem,
-            }}
-          >
-            <ListItemIcon>
-              <VideoLibraryIcon />
-            </ListItemIcon>
-            <ListItemText
-              primary="Biblioteca"
-              classes={{
-                primary: classes.DrawerListItemText,
-              }}
-            />
-          </ListItem>
-          <ListItem
-            button
-            classes={{
-              root: classes.DrawerListItem,
-            }}
-          >
-            <ListItemIcon>
-              <HistoryIcon />
-            </ListItemIcon>
-            <ListItemText
-              primary="Histórico"
-              classes={{
-                primary: classes.DrawerListItemText,
-              }}
-            />
-          </ListItem>
-        </List>
-        <Divider />
-        <Box p={3}>
-          <Typography variant="body2">
-            Faça login para curtir vídeos, comentar e se increver.
-          </Typography>
-          <Box mt={2}>
-            <Button
-              variant="outlined"
-              color="secondary"
-              startIcon={<AccountCircleIcon />}
             >
-              Fazer login
-            </Button>
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: classes.DrawerListItemText,
+                }}
+                primary="Início"
+              />
+            </ListItem>
+            <ListItem
+              button
+              classes={{
+                root: classes.DrawerListItem,
+              }}
+            >
+              <ListItemIcon>
+                <SubscriptionsIcon />
+              </ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: classes.DrawerListItemText,
+                }}
+                primary="Em alta"
+              />
+            </ListItem>
+            <ListItem
+              button
+              classes={{
+                root: classes.DrawerListItem,
+              }}
+            >
+              <ListItemIcon>
+                <WhatshotIcon />
+              </ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: classes.DrawerListItemText,
+                }}
+                primary="Inscrições"
+              />
+            </ListItem>
+          </List>
+          <Divider />
+          <List>
+            <ListItem
+              button
+              classes={{
+                root: classes.DrawerListItem,
+              }}
+            >
+              <ListItemIcon>
+                <VideoLibraryIcon />
+              </ListItemIcon>
+              <ListItemText
+                primary="Biblioteca"
+                classes={{
+                  primary: classes.DrawerListItemText,
+                }}
+              />
+            </ListItem>
+            <ListItem
+              button
+              classes={{
+                root: classes.DrawerListItem,
+              }}
+            >
+              <ListItemIcon>
+                <HistoryIcon />
+              </ListItemIcon>
+              <ListItemText
+                primary="Histórico"
+                classes={{
+                  primary: classes.DrawerListItemText,
+                }}
+              />
+            </ListItem>
+          </List>
+          <Divider />
+          <Box p={3}>
+            <Typography variant="body2">
+              Faça login para curtir vídeos, comentar e se increver.
+            </Typography>
+            <Box mt={2}>
+              <Button
+                variant="outlined"
+                color="secondary"
+                startIcon={<AccountCircleIcon />}
+              >
+                Fazer login
+              </Button>
+            </Box>
           </Box>
         </Box>
-      </Box>
-    </Drawer>
+      </Drawer>
+    </Hidden>
   );
 }
